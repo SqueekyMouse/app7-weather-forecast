cb4e76c471d58f32cfa9dd7e10ac44f7651f3ece
sreejith <squeeky.mouse@gmail.com>
2023-02-24 19:10:11 +0530

    plotly: plot fake data dynamically Sec32

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
-#commit: add widgets Sec32
+import plotly.express as px
+#commit: plotly: plot fake data dynamically Sec32
 
 st.title('Weather forecast for the next days')
 place=st.text_input('Place: ')
@@ -9,5 +10,16 @@ option=st.selectbox('Select data to view',
                     ('Temparature','Sky'))
 st.subheader(f'{option} for the next {days} days in {place}')
 
+def get_data(numdays):
+    # x and y shd be arrays of same size!!!
+    dates=['2022-25-10','2022-26-10','2022-27-10']
+    temperatures=[10,11,15]
+    # make the fig dynamic!!!
+    temperatures=[numdays*i for i in temperatures]
+    return(dates,temperatures)
 
-# print('hello world')
+d,t=get_data(days)
+
+# x and y shd be arrays of same size!!!
+figure=px.line(x=d,y=t,labels={'x':'Dates','y':'Temparature (C)'})
+st.plotly_chart(figure_or_data=figure)
