f2af51d8ca6ee38b6f6d23bc204ab0971a22cbfe
sreejith <squeeky.mouse@gmail.com>
2023-02-25 01:17:01 +0530

    done tempr graph and sky pics err handled Sec33

--- backend.py
+++ backend.py
@@ -7,23 +7,20 @@ API_KEY='664130abedff50a83c4c42004189fc01'
 # http://api.openweathermap.org/data/2.5/forecast?q=tokyo&appid=664130abedff50a83c4c42004189fc01
 # http://api.openweathermap.org/data/2.5/forecast?q=Tokyo&appid=2b0b3a2322e55cfbb245ec2dba4bb45b
 
-def get_data(place,forecast_days,kind):
-    url=f'http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}'
-    response=requests.get(url)
-    data=response.json()
-    filtered_data=data['list']
-    # len(filtered_data) #40
-    # 3hr data point 24/3=8 and 8*5=40
-    # 8 val for 24 hrs, 40 values for 5 days
-    num_of_values=8*forecast_days
-    filtered_data=filtered_data[:num_of_values]
-    if kind=='Temparature':
-        #get list of temparatures
-        filtered_data=[dict['main']['temp'] for dict in filtered_data]
-    if kind=='Sky':
-        #get list of sky condition
-        filtered_data=[dict['weather'][0]['main'] for dict in filtered_data]
+def get_data(place,forecast_days):
+    try:
+        url=f'http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}'
+        response=requests.get(url)
+        data=response.json()
+        filtered_data=data['list']
+        # len(filtered_data) #40
+        # 3hr data point 24/3=8 and 8*5=40 ie.8 val for 24 hrs,40 values for 5 days
+        num_of_values=8*forecast_days
+        filtered_data=filtered_data[:num_of_values]
+    except KeyError:
+        filtered_data=None
+    
     return(filtered_data)
 
 if __name__=='__main__':
-    print(get_data(place='Tokyo',forecast_days=3,kind='Temparature'))
+    print(get_data(place='Tokyo',forecast_days=3))
--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import streamlit as st
 import plotly.express as px
 from backend import get_data
-#commit: plotly: plot fake data dynamically Sec32
+#commit: done temperature graph and sky pics err handled Sec33
 
+# add title, text input, slider,selectbox and subheader
 st.title('Weather forecast for the next days')
-place=st.text_input('Place: ')
+place=st.text_input(label='Place: ')
+# place=st.text_input(label='Place: ',value='Tokyo')
 days=st.slider('Forecast Days',min_value=1,max_value=5,
                help='Select the number of days of forecast days')
 option=st.selectbox('Select data to view',
-                    ('Temparature','Sky'))
-st.subheader(f'{option} for the next {days} days in {place}')
+                    ('Temperature','Sky'))
 
-data=get_data(place,days,option)
-
-d,t=get_data(place)
-
-# x and y shd be arrays of same size!!!
-figure=px.line(x=d,y=t,labels={'x':'Dates','y':'Temparature (C)'})
-st.plotly_chart(figure_or_data=figure)
+if place:
+    # get the temparature or sky data
+    filtered_data=get_data(place,days)
+    if filtered_data==None:
+        st.info('Please recheck the City Name')
+    elif option=='Temperature':
+        st.subheader(f'{option} for the next {days} days in {place}')
+        temperatures=[dict['main']['temp']/10 for dict in filtered_data]
+        # print(temperatures)
+        dates=[dict['dt_txt'] for dict in filtered_data]
+        # create temparature plot
+        figure=px.line(x=dates,y=temperatures,labels={'x':'Dates','y':'Temperature (C)'})
+        st.plotly_chart(figure_or_data=figure)
+    elif option=='Sky':
+        sky_conditions=[dict['weather'][0]['main'] for dict in filtered_data]
+        images={'Clear':'images/clear.png','Clouds':'images/cloud.png',
+                'Snow':'images/snow.png','Rain':'images/rain.png'}
+        sky_conditions=[images[condition] for condition in sky_conditions]
+        # print(sky_conditions)
+        st.image(sky_conditions,width=115)
