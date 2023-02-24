85c14e3bd52c3b692e437e3c35e0bdb02569badf
sreejith <squeeky.mouse@gmail.com>
2023-02-24 18:14:40 +0530

    add widgets Sec32

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-#commit: Initial commit Sec32
+import streamlit as st
+#commit: add widgets Sec32
 
-print('hello world')
+st.title('Weather forecast for the next days')
+place=st.text_input('Place: ')
+days=st.slider('Forecast Days',min_value=1,max_value=5,
+               help='Select the number of days of forecast days')
+option=st.selectbox('Select data to view',
+                    ('Temparature','Sky'))
+st.subheader(f'{option} for the next {days} days in {place}')
+
+
+# print('hello world')
